adf776687531820f0c4e12bede1e012c29f84c64
MaksStudio <maxrognov23@gmail.com>
2024-05-14 11:55:58 +0300

    1.0.1

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 // Указываем Express использовать папку 'dist' для статических файлов
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
     // Отправляем файл 'index.html' при запросе к корню
     res.sendFile(path.join(__dirname,  'dist', 'index.html'));
 });
