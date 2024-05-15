b32feedc1b33348b7f517cfed52aff4bf3e89b6f
MaksStudio <maxrognov23@gmail.com>
2024-05-15 09:47:01 +0300

    1.0.2

--- index.js
+++ index.js
@@ -1,15 +1,47 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
 const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"]
+    }
+  }); 
+const path=require('path');
+const cors = require('cors'); // Импорт модуля cors
 
-// Указываем Express использовать папку 'dist' для статических файлов
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('*', (req, res) => {
-    // Отправляем файл 'index.html' при запросе к корню
-    res.sendFile(path.join(__dirname,  'dist', 'index.html'));
-});
-// Прослушивание порта 1488
-app.listen(8411, () => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html')); // Загрузка главного файла HTML вашего Vue.js приложения
+  });
+  
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+  });
+  
+  server.listen(8411, () => {
     console.log("Server is running on port 1488");
+});
+
+var connections = [];
+var online=0; 
+io.on('connection', (socket) => {
+    connections.push(socket);
+    online ++;
+    console.log("on" + online);
+    
+    io.sockets.emit('onlines', online);
+
+    socket.on('disconnect', (data) => {
+        connections.splice(connections.indexOf(socket), 1);
+        online --;
+        console.log("Off" + online);
+        io.sockets.emit('onlines', online); 
+    });
 });
